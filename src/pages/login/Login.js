81ddffe7cf906b9f1c40483822c890b13807f91a
seo yeon <lsy0476@naver.com>
2024-01-27 20:02:10 +0900

    feat: 로그인 구현

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {useNavigate} from 'react-router-dom';
+import '../../firebase.js'
 
-const Login = () => (
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSignIn =()=>{
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+      const user = userCredential.user;
+      console.log('login');
+      navigate('/rooms');
+      return user;
+    })
+    .catch((error)=>{
+      const errorMessage = error.message;
+      setError(errorMessage);
+    })
+  }
+
+  return (
     <>
-      <div>room</div>
+      <div>
+        <h1>Log In</h1>
+      </div>
+      <div>
+        <form>
+          <label>Email : </label>
+          <input 
+            type="email"
+            value={email}
+            onChange = {(e)=> setEmail(e.target.value)}
+          />
+          
+          <label>Password : </label>
+          <input
+            type="password"
+            value = {password}
+            onChange={(e)=>setPassword(e.target.value)}
+          />
+        </form>
+        <br/>
+        <button type="button" onClick={handleSignIn}>
+            Log In
+        </button>
+
+        {error && <p style = {{color : "red"}}>{error}</p>}
+      </div>
     </>
   );
+}
   
   export default Login;
   
