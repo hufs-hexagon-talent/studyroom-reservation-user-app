7ad46938d4a11e14ae150de5bc1bba9a6710ad83
yiseoffline <lsy0476@naver.com>
2024-01-28 20:32:11 +0900

    feat : create button in room.js

--- src/pages/rooms/roomId/Room.js
+++ src/pages/rooms/roomId/Room.js
@@ -1,10 +1,62 @@
 import { useParams } from "react-router-dom";
+import Button from '../../../components/Button';
 
 const Room = () => {
-    const {roomId} = useParams()
-    return (<>
-        <div>roomId: {roomId}</div>
-    </>)
+    const {roomId} = useParams();
+    var pageContent;
+
+    switch (roomId) {
+        case '306':
+            pageContent = <>
+                <Button text="room 1" onClick={() => {
+                    console.log("select room1");
+                }}
+                />
+                <br/>
+                <br/>
+                <Button text="room 2" onClick={() => {
+                    console.log("select room2");
+                }}
+                />
+                <br/>
+                <br/>
+                <Button text="room 3" onClick={() => {
+                    console.log("select room3");
+                }}
+                />
+                <br/>
+                <br/>
+                <Button text="room 4" onClick={() => {
+                    console.log("select room4");
+                }}
+                />
+            </>
+            break;
+        case '428':
+            pageContent = <>
+                <Button text="room 1" onClick={() => {
+                    console.log("select room1");
+                }}
+                />
+                <br/>
+                <br/>
+                <Button text="room 2" onClick={() => {
+                    console.log("select room2");
+                }}
+                />
+            </>
+            break;
+        default:
+            pageContent = <div> this is the default content for unknown rooms</div>
+    }
+
+    return (
+        <>
+            <div>{roomId} 호</div>
+            <br/>
+            {pageContent}
+        </>
+    );
 };
 
 export default Room;
