1f16f755d98fd5a6a7df2ec5ae324a59f4de2e8b
Jake Seunghyun Cho <shn07102@gmail.com>
2024-02-01 02:30:47 +0900

    column modified

--- src/pages/studyroom/roomId/Timetable.js
+++ src/pages/studyroom/roomId/Timetable.js
@@ -13,6 +13,10 @@ import './Timetable.css';
 
 import Button from '../../../components/Button';
 
+const TableCellWrapper = ({ children }) => (
+  <TableCell style={{ border: '1px solid #ddd' }}>{children}</TableCell>
+);
+
 const Timetable = () => {
   const today = new Date();
 
@@ -24,16 +28,14 @@ const Timetable = () => {
   const [selectedCells, setSelectedCells] = useState({});
   const [countClick, setCountClick] = useState(1);
 
-  // 셀을 클릭할 때 해당 셀의 선택 여부를 업데이트하는 함수 추가
   const handleCellClick = (room, hour) => {
-    // 클릭한 셀의 정보 출력
     console.log(`Clicked cell: Room ${room}, Hour ${hour}`);
   
     if (selectedCells[`${room}-${hour}`]) {
       setSelectedCells((prevSelectedCells) => {
         setCountClick(countClick - 1);
         const updatedCells = { ...prevSelectedCells };
-        delete updatedCells[`${room}-${hour}`]; // 선택 취소
+        delete updatedCells[`${room}-${hour}`];
         return updatedCells;
       });
     } else {
@@ -58,11 +60,10 @@ const Timetable = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell align="center" width={100} />
-              {/* 30분 간격으로 시간을 표시 */}
+              <TableCell style={{ border: '1px solid #ddd' }} align="center" width={100} />
               {[8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22].map(
                 hour => (
-                  <TableCell key={hour} align="center" width={200}>
+                  <TableCell key={hour} align="center" width={200} style={{ border: '1px solid #ddd' }}>
                     {`${Math.floor(hour)}:${(hour % 1) * 60 === 0 ? '00' : '30'}`}
                   </TableCell>
                 ),
@@ -72,14 +73,10 @@ const Timetable = () => {
           <TableBody>
             {['room1', 'room2', 'room3', 'room4'].map(room => (
               <TableRow key={room}>
-                <TableCell>{room}</TableCell>
-                {/* 30분 간격으로 셀을 생성 */}
+                <TableCellWrapper>{room}</TableCellWrapper>
                 {[8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22].map(
                   hour => (
-                    <TableCell
-                      key={`${room}-${hour}`} // 방이랑 시간 조합해서 키 값 생성\
-                      onClick={() => handleCellClick(room, hour)} // 클릭 이벤트 추가
-                    />
+                    <TableCellWrapper key={`${room}-${hour}`} onClick={() => handleCellClick(room, hour)} />
                   ),
                 )}
               </TableRow>
@@ -87,7 +84,6 @@ const Timetable = () => {
           </TableBody>
         </Table>
       </TableContainer>
-
 
       <br />
       <Button
