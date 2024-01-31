9ac6afd9629487d4c8d2e88f81935f258eedf5c2
yiseoffline <lsy0476@naver.com>
2024-01-31 17:50:22 +0900

    change interval 30 minutes

--- src/pages/studyroom/roomId/Timetable.js
+++ src/pages/studyroom/roomId/Timetable.js
@@ -43,58 +43,61 @@ const Room = () => {
   return (
     <>
       <TableContainer
-        sx={{
-          width: '90%',
-          minWidth: '650px',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          marginTop: '50px',
-        }}>
-        <Typography variant="h5" fontWeight={10} component="div" align="center">
-          예약하기
-        </Typography>
-        <div>
-          {year}년 {month}월 {day}일
-        </div>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" width={100} />
-              {[8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22].map(
-                hour => (
-                  <TableCell key={hour} align="center" width={200}>
-                    {`${hour}:00`}
-                  </TableCell>
-                ),
-              )}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {['room1', 'room2', 'room3', 'room4'].map(room => (
-              <TableRow key={room}>
-                <TableCell>{room}</TableCell>
-                {[8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22].map(
-                  hour => (
-                    <TableCell
-                      key={`${room}-${hour}`}
-                      align="center"
-                      width={200}
-                      sx={{
-                        borderLeft: '1px solid #ccc',
-                        backgroundColor: selectedCells[`${room}-${hour}`]
-                          ? '#4B89DC'
-                          : 'transparent', // 선택된 셀에 따라 색상 변경
-                        cursor: 'pointer', // 클릭 가능한 커서 스타일 추가
-                      }}
-                      onClick={() => handleCellClick(room, hour)} // 클릭 이벤트 추가
-                    />
-                  ),
-                )}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+  sx={{
+    width: '90%',
+    minWidth: '650px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '50px',
+  }}
+>
+  <Typography variant="h5" fontWeight={10} component="div" align="center">
+    예약하기
+  </Typography>
+  <div>
+    {year}년 {month}월 {day}일
+  </div>
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell align="center" width={100} />
+        {/* 30분 간격으로 시간을 표시 */}
+        {[8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22].map(
+          hour => (
+            <TableCell key={hour} align="center" width={200} padding='8px'>
+              {`${Math.floor(hour)}:${(hour % 1) * 60 === 0 ? '00' : '30'}`}
+            </TableCell>
+          ),
+        )}
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {['room1', 'room2', 'room3', 'room4'].map(room => (
+        <TableRow key={room}>
+          <TableCell>{room}</TableCell>
+          {/* 30분 간격으로 셀을 생성 */}
+          {[8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22].map(
+            hour => (
+              <TableCell
+                key={`${room}-${hour}`} // 방이랑 시간 조합해서 키 값 생성
+                sx={{
+                  borderLeft: '1px solid #ccc',
+                  borderRight: '1px solid #ccc',
+                  backgroundColor: selectedCells[`${room}-${hour}`]
+                    ? '#4B89DC'
+                    : 'transparent', // 선택된 셀에 따라 색상 변경
+                  cursor: 'pointer', // 클릭 가능한 커서 스타일 추가
+                }}
+                onClick={() => handleCellClick(room, hour)} // 클릭 이벤트 추가
+              />
+            ),
+          )}
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+</TableContainer>
+
 
       <br />
       <Button
