8611cc684b84059513ee1223470878102ddf5214
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-01 02:17:13 +0900

    upload Timetable

--- src/pages/studyroom/roomId/Timetable.js
+++ src/pages/studyroom/roomId/Timetable.js
@@ -24,14 +24,16 @@ const Timetable = () => {
   const [selectedCells, setSelectedCells] = useState({});
   const [countClick, setCountClick] = useState(1);
 
+  // 셀을 클릭할 때 해당 셀의 선택 여부를 업데이트하는 함수 추가
   const handleCellClick = (room, hour) => {
+    // 클릭한 셀의 정보 출력
     console.log(`Clicked cell: Room ${room}, Hour ${hour}`);
-
+  
     if (selectedCells[`${room}-${hour}`]) {
       setSelectedCells((prevSelectedCells) => {
         setCountClick(countClick - 1);
         const updatedCells = { ...prevSelectedCells };
-        delete updatedCells[`${room}-${hour}`];
+        delete updatedCells[`${room}-${hour}`]; // 선택 취소
         return updatedCells;
       });
     } else {
@@ -46,30 +48,19 @@ const Timetable = () => {
   };
 
   return (
-    <div>
-      <div className='reserve'>
-        <h1>예약하기</h1>
-      </div>
-      <div>
-        {year}년 {month}월 {day}일
-      </div>
-      <TableContainer
-        sx={{
-          width: '90%',
-          minWidth: '650px',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          marginTop: '50px',
-        }}
-      >
+    <>
+      <TableContainer>
+        <div className="dayText">
+          <span className='textStyle'>
+          {year}년 {month}월 {day}일
+          </span>
+        </div>
         <Table>
           <TableHead>
             <TableRow>
               <TableCell align="center" width={100} />
-              {[
-                8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17,
-                17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22
-              ].map(
+              {/* 30분 간격으로 시간을 표시 */}
+              {[8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22].map(
                 hour => (
                   <TableCell key={hour} align="center" width={200}>
                     {`${Math.floor(hour)}:${(hour % 1) * 60 === 0 ? '00' : '30'}`}
@@ -82,12 +73,12 @@ const Timetable = () => {
             {['room1', 'room2', 'room3', 'room4'].map(room => (
               <TableRow key={room}>
                 <TableCell>{room}</TableCell>
-                {[8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5,
-                  18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22].map(
+                {/* 30분 간격으로 셀을 생성 */}
+                {[8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22].map(
                   hour => (
                     <TableCell
-                      key={`${room}-${hour}`}
-                      onClick={() => handleCellClick(room, hour)}
+                      key={`${room}-${hour}`} // 방이랑 시간 조합해서 키 값 생성\
+                      onClick={() => handleCellClick(room, hour)} // 클릭 이벤트 추가
                     />
                   ),
                 )}
@@ -104,7 +95,7 @@ const Timetable = () => {
           navigate('/reservation');
         }}
       />
-    </div>
+    </>
   );
 };
 
