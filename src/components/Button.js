ee540091b399cb8b3b6cb4107fc24ae40e9cc0d9
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-31 20:05:20 +0900

    button style modified

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
+import '../components/Button.css';
+
 const Button = ({ text, onClick }) => {
   return (
-    <p
-      className="bg-gray-800 hover:bg-gray-600 duration-100 text-white inline m-2 p-2 rounded-lg cursor-pointer"
-      onClick={onClick}
-      style={{
-        backgroundColor: '#EDBC6F',
-        color: 'black',
-        width: '200%',
-        height: '200%',
-      }}>
+    <p className="btn"
+    onClick={onClick}
+    >
       {text}
     </p>
   );
