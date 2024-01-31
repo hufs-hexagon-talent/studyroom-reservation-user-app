d79e4d7eea7cd4322e925385e1edb3f07725c2a1
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-01-31 22:22:02 +0900

    nav modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="navbar">
         <img src={logo} alt="" className="logo" />
         <div className="titleName">컴퓨터공학부 스터디룸 예약 시스템</div>
-        <ul>
+        <ul className="navLinks">
           <li>
             <a href="/">예약 현황</a>
           </li>
