6dcba6c8508cc238c35cef70f569ca11d3e9e18c
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-10 20:10:46 +0900

    button style change to tailwind

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import '../components/Button.css';
-
 const Button = ({ text, onClick }) => {
   return (
-    <div className='boxSize'>
-      <p className="btn"
-      onClick={onClick}
-      >
+    <div className="w-40 mx-auto">
+      {' '}
+      {/* margin left, right auto */}
+      <p
+        className="text-center text-white bg-yellow-400 hover:bg-red-700 text-base px-4 py-2 rounded-full focus:outline-none"
+        onClick={onClick}>
         {text}
       </p>
     </div>
