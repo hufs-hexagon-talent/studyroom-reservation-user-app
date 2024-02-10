dd362aa1b05be17bd62754f9416475face0cf3d9
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-10 20:17:15 +0900

    unnecessary files deleted

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Button = ({ text, onClick }) => {
   return (
-    <div className="w-40 mx-auto">
-      {' '}
+    <div className="w-40 py-1 mx-auto">
       {/* margin left, right auto */}
       <p
         className="text-center text-white bg-yellow-400 hover:bg-red-700 text-base px-4 py-2 rounded-full focus:outline-none"
