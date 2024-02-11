5fc42f2f65b6865c215b52fa333e42b9fe7a5c75
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-11 13:00:21 +0900

    login style modified (#8)
    * LoginPage.jsx wrapper
    
    * 수정중
    
    * login page style modified

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -29,31 +29,37 @@ const LoginPage = () => {
 
   return (
     <>
-      <div>
-        <h1>Log In</h1>
-      </div>
-      <div>
-        <form>
-          <label>Email : </label>
-          <input
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
+      <div className="h-screen flex items-center justify-center bg-gray-800">
+        <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+          <h3 className="text-3xl text-gray-800">Log In</h3>
+          <div>
+            <form className="flex flex-col mt-5 px-5">
+              <input
+                placeholder="Email"
+                className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
 
-          <label>Password : </label>
-          <input
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </form>
-        <br />
-        <button type="button" onClick={handleSignIn}>
-          Log In
-        </button>
+              <input
+                placeholder="Password"
+                className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </form>
+            <button
+              className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90"
+              type="button"
+              onClick={handleSignIn}>
+              Log In
+            </button>
 
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+          </div>
+        </div>
       </div>
     </>
   );
