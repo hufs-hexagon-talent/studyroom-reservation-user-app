aca2a0ecbf330e7f5875e99340abef3fdcf08a25
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-11 13:22:44 +0900

    conflict

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -34,47 +34,50 @@ const LoginPage = () => {
           <h3 className="text-3xl text-gray-800">Log In</h3>
           <div>
             <form className="flex flex-col mt-5 px-5">
-              <input
-                placeholder="Email"
-                className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-yellow-400 mb-3 py-3 px-5 rounded-lg"
-                type="email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              <div>
+                <input
+                  placeholder="Email"
+                  className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-yellow-400 mb-3 py-3 px-5 rounded-lg"
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
                 />
-              </svg>
-
-              <input
-                placeholder="Password"
-                className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-yellow-400 mb-3 py-3 px-5 rounded-lg"
-                type="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </div>
+              <div>
+                <input
+                  placeholder="Password"
+                  className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-yellow-400 mb-3 py-3 px-5 rounded-lg"
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
                 />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
+                </svg>
+              </div>
             </form>
 
             <button
