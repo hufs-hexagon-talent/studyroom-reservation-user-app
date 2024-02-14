014307ef1bb4063ae7d32a01d2f5701ccf03cc47
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-15 02:47:35 +0900

    box size up

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Button = ({ text, onClick }) => {
   return (
-    <div className="w-40 py-1 mx-auto">
+    <div className="w-60 py-3 mx-auto">
       {/* margin left, right auto */}
       <p
-        className="text-center text-white bg-yellow-400 hover:bg-red-700 text-base px-4 py-2 rounded-full focus:outline-none"
+        className="align-middle text-center text-white bg-yellow-400 hover:bg-red-700 text-2xl px-4 py-2 rounded-full focus:outline-none cursor-pointer"
         onClick={onClick}>
         {text}
       </p>
