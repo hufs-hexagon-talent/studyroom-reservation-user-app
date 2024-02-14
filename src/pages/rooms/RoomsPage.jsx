c2fc20646d147786164961ffc00beb4c109c4ba3
Jake Seunghyun Cho <shn07102@gmail.com>
2024-02-14 22:11:50 +0900

    feat:회색

--- src/pages/rooms/RoomsPage.jsx
+++ src/pages/rooms/RoomsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
 
 import Button from '../../components/Button';
 import { fs } from '../../firebase';
@@ -40,19 +40,19 @@ const RoomsPage = () => {
       console.log('error');
     }
   };
-  const isSlotReserved = async (roomId, slotIndex) => {
-    try {
-      const q = query(
-        collection(fs, `Rooms/${roomId}/Days/${roomId}/Reservations`),
-        where('slotIndex', '==', slotIndex), // 예약된 슬롯의 인덱스와 일치하는 예약 문서를 찾습니다.
-      );
-      const querySnapshot = await getDocs(q);
-      return !querySnapshot.empty; // 예약된 슬롯이 존재하면 true를 반환합니다.
-    } catch (error) {
-      console.error('Error checking reservation:', error);
-      return false; // 예외 발생 시 기본적으로 예약되지 않은 것으로 처리합니다.
-    }
-  };
+  // const isSlotReserved = async (roomId, slotIndex) => {
+  //   try {
+  //     const q = query(
+  //       collection(fs, `Rooms/${roomId}/Days/${roomId}/Reservations`),
+  //       where('slotIndex', '==', slotIndex), // 예약된 슬롯의 인덱스와 일치하는 예약 문서를 찾습니다.
+  //     );
+  //     const querySnapshot = await getDocs(q);
+  //     return !querySnapshot.empty; // 예약된 슬롯이 존재하면 true를 반환합니다.
+  //   } catch (error) {
+  //     console.error('Error checking reservation:', error);
+  //     return false; // 예외 발생 시 기본적으로 예약되지 않은 것으로 처리합니다.
+  //   }
+  // };
 
   return (
     <>
