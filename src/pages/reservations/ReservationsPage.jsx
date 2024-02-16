62a3e62abac6a7aea2b1933632664708ab23056e
yiseoffline <lsy0476@naver.com>
2024-02-16 22:04:16 +0900

    feat : change checkroom.jsx structure
    table -> box

--- src/pages/reservations/ReservationsPage.jsx
+++ src/pages/reservations/ReservationsPage.jsx
@@ -30,33 +30,48 @@ const ReservationsPage = () => {
 
   return (
     <>
-      <div>
-        <h1 className="py-10 text-3xl text-center font-bold">
-          Reserved!
-        </h1>
-        <div className='flex justify-center items-center'>
-          <div className='w-48 h-48'>
-            <img src={boo} alt='boo'/>
+      {reservationData ? (
+        <div>
+          <h1 className="py-10 text-2xl text-center font-bold">
+            {reservationData.userName}님, <br/>예약 되었습니다!
+          </h1>
+          <div className='flex justify-center items-center'>
+            <div className='w-48 h-48'>
+              <img src={boo} alt='boo'/>
+            </div>
           </div>
+          <div className="flex justify-center items-center">
+            <div className="bg-blue-200 w-full max-w-lg py-10 rounded-lg text-center">
+              <p className="text-xl font-semibold">예약 내역</p>
+              <br />
+              <p>
+                이름: {reservationData.userName}<br />
+                호실: {reservationData.roomName}호 {reservationData.partitionName}<br />
+                시간: {reservationData.startTimeString} - {reservationData.endTimeString}
+              </p>
+            </div>
+          </div>
+          <br/>
+          <Button
+            text={'완료'}
+            onClick={()=>{
+              navigate('/');
+            }}
+          />
+          <br/>
+          <Button 
+            text={'예약 현황 둘러보기'}
+            onClick={() => {
+              navigate('/status');
+            }}
+          />
         </div>
-
-        <div className="text-center">
-          {reservationData && (
-            <>
-              {reservationData.userName}님 {reservationData.roomName}호 {reservationData.partitionName} {reservationData.startTimeString}시 ~ {reservationData.endTimeString}시 예약되었습니다.
-            </>
-          )}
-        </div>
-      </div>
-      <br/>
-      <Button 
-        text={'예약 현황 둘러보기'}
-        onClick={() => {
-          navigate('/status');
-        }}
-      />
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
+  
 };
 
 export default ReservationsPage;
