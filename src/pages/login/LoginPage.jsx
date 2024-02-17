ae37ceb07366ba5819f3a9b79d06ff758a7c01b1
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-17 18:24:58 +0900

    기본틀

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -4,9 +4,6 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 import '../../firebase.js';
 
-import boo from '../../assets/boo.jpeg';
-
-// practice
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -30,43 +27,34 @@ const LoginPage = () => {
   };
 
   return (
-    <>
-      <div className="p-5 flex justify-center items-center">
-        <div className="w-48 h-48">
-          <img src={boo} alt="boo" />
+    <div>
+      {' '}
+      {/**container flex */}
+      <div>
+        {' '}
+        {/** */}
+        <div>
+          {' '}
+          {/** */}
+          <h1>LOGIN</h1>
+          <p>이용하시기 전에 먼저 로그인해주세요</p>
         </div>
+        <form>
+          <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </form>
       </div>
-      <div className="flex items-center justify-center">
-        <div className="bg-blue-200 w-3/5 py-10 rounded-lg text-center">
-          <h3 className="text-3xl text-gray-800">Log In</h3>
-          <div className="mt-5 px-5">
-            <form className="flex flex-col">
-              <input
-                placeholder="Email"
-                className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-                type="email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-              />
-              <input
-                placeholder="Password"
-                className="bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-50 focus:border-green-600 mb-3 py-3 px-5 rounded-lg"
-                type="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-              />
-            </form>
-            <button
-              className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90"
-              type="button"
-              onClick={handleSignIn}>
-              Log In
-            </button>
-            {error && <p className="text-red-500 mt-3">{error}</p>}
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
