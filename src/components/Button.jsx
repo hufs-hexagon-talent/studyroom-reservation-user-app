8a013a2dd29710e22920ff7b23a8239572460365
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-02-17 19:04:06 +0900

    status update (#16)
    * 기본틀
    
    * status upload

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Button = ({ text, onClick }) => {
   return (
-    <div className="w-60 py-4 mx-auto">
+    <div className="w-80 py-3.5 mx-auto">
       {/* margin left, right auto */}
       <p
         className="h-20 text-center py-5 align-middle text-white 
-          bg-blue-300 hover:bg-gray-400 text-2xl rounded-full 
-          focus:outline-none cursor-pointer"
+          bg-gray-800 text-2xl rounded-full 
+          focus:outline-none cursor-pointer hover:opacity-90"
         onClick={onClick}>
         {text}
       </p>
