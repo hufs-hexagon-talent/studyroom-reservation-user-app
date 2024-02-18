f79c0de385341acdd61d83fae3239f2f2575539b
Jake Seunghyun Cho <shn07102@gmail.com>
2024-02-18 18:33:31 +0900

    status-modify

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -9,7 +9,7 @@ import login_boo from '../../assets/login_boo.jpg';
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
+  const [setError] = useState(null);
   const navigate = useNavigate();
 
   const handleSignIn = () => {
