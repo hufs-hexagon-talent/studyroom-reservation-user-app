41abeb8cd491e7a6763a75b3f1e1d189791bc3a5
yiseoffline <lsy0476@naver.com>
2024-03-10 20:12:56 +0900

    change icon & h-screen

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import { HiChartPie, HiInbox, HiUser, HiViewBoards } from 'react-icons/hi';
+import { BiCalendarEdit,BiDetail,BiLogIn } from "react-icons/bi";
+import { IoPersonSharp } from "react-icons/io5";
 import { Sidebar } from 'flowbite-react';
 
 const SideBar = () => {
   return (
-    <Sidebar aria-label="Default sidebar example" className="flex">
-      <Sidebar.Logo href="/" img="/img/logo.png" imgAlt="Logo">
+    <Sidebar aria-label="Default sidebar example" className="flex h-screen">
+      <Sidebar.Logo href="/" img="/img/logo.png" imgAlt="Logo" style={{height: '50px'}}>
         컴퓨터공학부 세미나실 예약 시스템
       </Sidebar.Logo>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item href="./rooms" icon={HiChartPie}>
+          <Sidebar.Item href="./rooms" icon={BiCalendarEdit}>
             세미나실 예약하기
           </Sidebar.Item>
-          <Sidebar.Item href="./check" icon={HiViewBoards}>
+          <Sidebar.Item href="./check" icon={IoPersonSharp}>
             내 신청 현황
           </Sidebar.Item>
-          <Sidebar.Item href="./notice" icon={HiInbox}>
+          <Sidebar.Item href="./notice" icon={BiDetail}>
             이용 규칙
           </Sidebar.Item>
-          <Sidebar.Item href="./login" icon={HiUser}>
+          <Sidebar.Item href="./login" icon={BiLogIn}>
             로그인
           </Sidebar.Item>
         </Sidebar.ItemGroup>
