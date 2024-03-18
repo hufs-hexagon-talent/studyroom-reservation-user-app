662c1642c4f9dfca5c81066f2edf7d97d0366eb7
yiseoffline <lsy0476@hufs.ac.kr>
2024-03-18 16:25:18 +0900

    feat : add popover & make responsive

--- src/pages/check/CheckRoom.jsx
+++ src/pages/check/CheckRoom.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+'use client';
+
+import React from 'react';
+import { Button, Popover, Typography } from '@mui/material';
+import { Table } from 'flowbite-react';
+
+import './CheckRoom.css';
 
 const Check = () => {
-  const [rooms, setRooms] = useState([]);
-
   const today = new Date();
   const year = today.getFullYear();
   const month = today.getMonth() + 1;
@@ -13,10 +17,77 @@ const Check = () => {
 
   const currentDay = `${year}.${monthFormatted}.${dayFormatted}`;
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <div>
-      <div className="felx text-center font-bold text-3xl mt-10">
+      <div className="felx text-center font-bold text-3xl mt-20">
         내 신청 현황
+      </div>
+
+      <div id="table" className="overflow-x-auto mt-10 p-2">
+        <Table className="border">
+          <Table.Head
+            style={{ fontSize: 15 }}
+            className="text-black text-center">
+            <Table.HeadCell>예약자</Table.HeadCell>
+            <Table.HeadCell>호실</Table.HeadCell>
+            <Table.HeadCell>
+              방 <br />
+              번호
+            </Table.HeadCell>
+            <Table.HeadCell>날짜</Table.HeadCell>
+            <Table.HeadCell>시간</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800 text-center text-gray-900">
+              <Table.Cell>이서연</Table.Cell>
+              <Table.Cell>306호</Table.Cell>
+              <Table.Cell>room 3</Table.Cell>
+              <Table.Cell>2024-03-12</Table.Cell>
+              <Table.Cell>15:00 - 17:00</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
+      <div id="popover">
+        <Button
+          style={{
+            backgroundColor: '#002D56',
+            marginLeft: 15,
+            marginTop: 20,
+          }}
+          aria-describedby={id}
+          variant="contained"
+          onClick={handleClick}>
+          노쇼 현황
+        </Button>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}>
+          <Typography sx={{ p: 2 }}>
+            * 현재 예약 취소 없이 세미나실을 방문하지 않은 횟수는 n번 입니다.
+            <br />
+            (3회 초과 시 세미나실 예약이 제한 됩니다)
+          </Typography>
+        </Popover>
       </div>
     </div>
   );
