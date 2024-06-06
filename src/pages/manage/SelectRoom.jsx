4bea51682f182414cf93d7d8ee1db4fa5d1bde52
sooo03 <107316965+sooo03@users.noreply.github.com>
2024-06-07 00:09:16 +0900

    column, row

--- src/pages/manage/SelectRoom.jsx
+++ src/pages/manage/SelectRoom.jsx
@@ -12,20 +12,41 @@ const SelectRoom = () => {
     return <div>Error loading rooms.</div>;
   }
 
+  // 방 번호에 따라 두 개의 열로 나누기
+  const firstColumnRooms = roomNames.filter(room => room.startsWith('306'));
+  const secondColumnRooms = roomNames.filter(room => room.startsWith('428'));
+
   return (
-    <div>
+    <div className="m-10">
       <div className="font-bold mt-10 text-2xl text-center">
         방을 선택하세요
       </div>
-      <div className="flex items-center justify-center mt-10">
-        <div className="flex flex-wrap items-start">
-          {roomNames.map((roomName, index) => (
-            <div className="flex items-center mb-4 mr-8" key={index}>
+      <div className="flex justify-center mt-4 space-x-8">
+        <div className="flex flex-col">
+          {firstColumnRooms.map((roomName, index) => (
+            <div className="flex items-center mb-4" key={index}>
               <input
                 id={`box-${roomName}`}
                 type="checkbox"
                 value={roomName}
-                className="w-4 h-4"
+                className="w-4 h-4 bg-gray-100 border-gray-300 rounded"
+              />
+              <label
+                htmlFor={`box-${roomName}`}
+                className="ml-2 text-xl font-medium">
+                {roomName}
+              </label>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col">
+          {secondColumnRooms.map((roomName, index) => (
+            <div className="flex items-center mb-4" key={index}>
+              <input
+                id={`box-${roomName}`}
+                type="checkbox"
+                value={roomName}
+                className="w-4 h-4 bg-gray-100 border-gray-300 rounded"
               />
               <label
                 htmlFor={`box-${roomName}`}
