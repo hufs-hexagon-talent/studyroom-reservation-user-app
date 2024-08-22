6bcd13627de42b626e8929dab4e1de3b86d91437
yiseoffline <lsy0476@hufs.ac.kr>
2024-08-22 20:20:03 +0900

    💄 change divide act structure

--- src/pages/manage/DivideAct.jsx
+++ src/pages/manage/DivideAct.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, Modal } from 'flowbite-react';
 
 const DivideAct = () => {
+  const [openModal, setOpenModal] = useState(true);
   const navigate = useNavigate();
+
   return (
     <div>
       <h3 className="flex font-bold justify-center w-screen text-2xl text-center mt-20 mb-5">
@@ -15,16 +18,33 @@ const DivideAct = () => {
           스케줄 설정하러 가기 &gt;
         </p>
         <p
-          onClick={() => navigate('/selectPartition')}
+          onClick={() => setOpenModal(true)}
           className="inline-block text-xl hover:underline cursor-pointer pb-10">
           예약 조회하러 가기 &gt;
         </p>
-        <p
-          onClick={() => navigate('/serialCheck')}
-          className="inline-block text-xl hover:underline cursor-pointer">
-          학번으로 예약 조회하러 가기 &gt;
-        </p>
       </div>
+      <Modal show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header>예약 조회 방법 선택</Modal.Header>
+        <Modal.Body>
+          <div className="flex flex-col space-y-6">
+            <p
+              onClick={() => navigate('/selectPartition')}
+              className="inline-block text-lg hover:underline cursor-pointer">
+              호실 및 날짜로 예약 조회하러 가기 &gt;
+            </p>
+            <p
+              onClick={() => navigate('/serialCheck')}
+              className="inline-block text-lg hover:underline cursor-pointer">
+              학번으로 예약 조회하러 가기 &gt;
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button color="gray" onClick={() => setOpenModal(false)}>
+            취소
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
--- src/pages/manage/SelectPartition.jsx
+++ src/pages/manage/SelectPartition.jsx
@@ -6,6 +6,7 @@ import { Button } from 'flowbite-react';
 const SelectRoom = () => {
   const { data: partitions, error, isLoading } = useAllPartitions();
   const [selectedPartitions, setSelectedPartitions] = useState([]);
+  const [isAllSelected, setIsAllSelected] = useState(false); // 전체 선택 상태 추가
   const navigate = useNavigate();
 
   if (isLoading) {
@@ -30,6 +31,18 @@ const SelectRoom = () => {
     });
   };
 
+  const handleSelectAllChange = () => {
+    if (isAllSelected) {
+      // 이미 전체 선택된 상태라면 선택을 모두 해제
+      setSelectedPartitions([]);
+      setIsAllSelected(false);
+    } else {
+      // 전체 선택되지 않은 상태라면 모든 파티션 선택
+      setSelectedPartitions(partitions);
+      setIsAllSelected(true);
+    }
+  };
+
   const handleNextClick = () => {
     const selectedPartitionIds = selectedPartitions.map(
       partition => partition.partitionId,
@@ -39,7 +52,7 @@ const SelectRoom = () => {
 
   return (
     <div className="p-10">
-      <div className="mt-20 text-2xl text-center">
+      <div className="mt-10 text-2xl text-center">
         예약 조회를 원하는 방을 선택하세요
       </div>
       <div className="flex justify-center mt-12">
@@ -51,6 +64,9 @@ const SelectRoom = () => {
                 type="checkbox"
                 value={partition.partitionId}
                 className="w-4 h-4 bg-gray-100 border-gray-300"
+                checked={selectedPartitions.some(
+                  p => p.partitionId === partition.partitionId,
+                )}
                 onChange={() => handleCheckboxChange(partition)}
               />
               <label
@@ -60,6 +76,18 @@ const SelectRoom = () => {
               </label>
             </div>
           ))}
+          <div className="flex items-center mb-4">
+            <input
+              id="select-all"
+              type="checkbox"
+              className="w-4 h-4 bg-gray-100 border-gray-300"
+              checked={isAllSelected}
+              onChange={handleSelectAllChange}
+            />
+            <label htmlFor="select-all" className="ml-2 text-xl">
+              전체 선택
+            </label>
+          </div>
         </div>
       </div>
       <div className="flex justify-end w-full mt-6">
