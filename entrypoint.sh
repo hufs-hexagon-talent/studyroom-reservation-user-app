6c00b8223e7f886ed99deb93937cd87c3aeab9b6
HwangBBang <h1009218@hufs.ac.kr>
2025-02-27 19:27:18 +0900

    ci: fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 set -Ex
 
 echo "Check that we have environment vars"
+echo "REACT_APP_API_URL is: $REACT_APP_API_URL"
 test -n "$REACT_APP_API_URL"
-find /usr/share/nginx/html \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP__REPLACE_ME__REACT_APP_API_URL#$REACT_APP_API_URL#g"
+
+find /usr/share/nginx/html \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i"" "s#APP__REPLACE_ME__REACT_APP_API_URL#$REACT_APP_API_URL#g"
 
 echo "Starting Nginx"
 exec "$@"
