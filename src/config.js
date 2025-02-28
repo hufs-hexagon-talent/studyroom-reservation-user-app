ba089fdcfe5dd994064edeb8ad4f3a0cbe156713
yiseoffline <lsy0476@hufs.ac.kr>
2025-02-28 13:54:32 +0900

    fix: add env in deploy.yml

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-  API_URL: window.env?.REACT_APP_API_URL || 'https://api.studyroom.alpaon.net',
+  API_URL:
+    window.env?.REACT_APP_API_URL ||
+    'https://api.studyroom.computer.hufs.ac.kr',
   DEPARTMENT_ID: window.env?.REACT_APP_DEPARTMENT_ID || 1,
 };
 
