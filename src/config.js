b5d10bf188360b6868f948bc2805da569e7c40d9
yiseoffline <lsy0476@hufs.ac.kr>
2025-05-21 19:10:52 +0900

    fix: main api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
   API_URL:
-    window.env?.REACT_APP_QA_API_URL || 'https://api.studyroom-qa.alpaon.net',
+    window.env?.REACT_APP_API_URL ||
+    'https://api.studyroom.computer.hufs.ac.kr',
   DEPARTMENT_ID: window.env?.REACT_APP_DEPARTMENT_ID || 1,
 };
 
